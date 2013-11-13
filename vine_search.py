b55470506ebd70572234b4fb2070cec24d8742a5
Sam Agnew <sagnew@eden.rutgers.edu>
2013-11-13 02:05:20 -0500

    Searches through tweets

---
+++ vine_search.py
@@ -0,0 +1,25 @@
+from constants import *
+from TwitterSearch import *
+
+def search_tweets(query):
+    try:
+        tso = TwitterSearchOrder()
+        tso.setKeywords([query])
+        tso.setLanguage('en')
+        tso.setCount(100)
+        tso.setIncludeEntities(False)
+
+        ts = TwitterSearch(
+            consumer_key = consumer_key,
+            consumer_secret = consumer_secret,
+            access_token = access_token,
+            access_token_secret = access_token_secret
+         )
+
+        tweets = []
+        for tweet in ts.searchTweetsIterable(tso):
+            tweets.append(tweet['text'])
+        return tweets
+
+    except TwitterSearchException as e:
+        return "No results"
